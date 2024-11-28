397f0b1d4798aa89aa8b22f633ac2f20585efd29
[Francisco Omedes] <omedesf@gmail.com>
2024-11-28 00:49:37 +0000

    radial backgrounds

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,14 @@ export default {
       animation: {
         // Bounces 5 times 1s equals 5 seconds
         'bounce-short': 'bounce 1s ease-in-out 3.5'
+      },
+      colors: {
+        'primary':'#BAA333',
+        'secondary':'#F0E8D8',
+        'bg-primary':'#F0E8D8',
+        'bg-secondary':'#F0E8D8',
+        'radial-bg-primary':'#CD02BC',
+        'radial-bg-secondary':'#450167',
       }
     },
   },
